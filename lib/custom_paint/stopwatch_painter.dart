1c3d7e8a7fa72dfa0462abc88f7df0afbca4b720
Sakari De-Meis <sakaride-meis@Sakaris-MacBook-Pro.local>
2021-01-28 21:44:31 +0100

    Create stopwatch painter

--- lib/custom_paint/stopwatch_painter.dart
+++ lib/custom_paint/stopwatch_painter.dart
@@ -3,12 +3,6 @@ import 'package:flutter/material.dart';
 import '.././constants/export.dart';
 
 class StopwatchPainter extends CustomPainter {
-  var dateTime = DateTime.now();
-
-  //60sec - 360, 1 sec - 6degrees
-  //60min - 360, 1 min - 6degrees
-  //12hours - 360, 1 hour - 30degrees, 60min - 30degrees, 1 min - 0.5degrees
-
   @override
   void paint(Canvas canvas, Size size) {
     var centerX = size.width / 2;
@@ -23,12 +17,6 @@ class StopwatchPainter extends CustomPainter {
       ..strokeWidth = size.width / 20;
     var centerDotBrush = Paint()..color = CustomColors.clockOutline;
 
-    var secHandBrush = Paint()
-      ..color = CustomColors.secHandColor
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = size.width / 60;
-
     var minHandBrush = Paint()
       ..shader = RadialGradient(colors: [
         CustomColors.minHandStatColor,
@@ -38,53 +26,24 @@ class StopwatchPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..strokeWidth = size.width / 30;
 
-    var hourHandBrush = Paint()
-      ..shader = RadialGradient(colors: [
-        CustomColors.hourHandStatColor,
-        CustomColors.hourHandEndColor
-      ]).createShader(Rect.fromCircle(center: center, radius: radius))
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = size.width / 24;
-
-    var dashBrush = Paint()
+    var line = Paint()
       ..color = CustomColors.clockOutline
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 1;
+      ..strokeWidth = 25;
+
+    canvas.drawLine(Offset(115, 10), Offset(155, 10), line);
+    canvas.drawLine(Offset(150, 120), Offset(50, 50), line);
+    canvas.drawLine(Offset(220, 50), Offset(170, 80), line);
 
     canvas.drawCircle(center, radius * 0.75, fillBrush);
     canvas.drawCircle(center, radius * 0.75, outlineBrush);
 
-    var hourHandX = centerX +
-        radius *
-            0.4 *
-            cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
-    var hourHandY = centerY +
-        radius *
-            0.4 *
-            sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
-    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
-
-    var minHandX = centerX + radius * 0.6 * cos(dateTime.minute * 6 * pi / 180);
-    var minHandY = centerY + radius * 0.6 * sin(dateTime.minute * 6 * pi / 180);
+    var minHandX = centerX + radius * 0.6 * cos(269 * pi / 180);
+    var minHandY = centerY + radius * 0.6 * sin(269 * pi / 180);
     canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
 
-    var secHandX = centerX + radius * 0.6 * cos(dateTime.second * 6 * pi / 180);
-    var secHandY = centerY + radius * 0.6 * sin(dateTime.second * 6 * pi / 180);
-    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
-
     canvas.drawCircle(center, radius * 0.12, centerDotBrush);
-
-    var outerRadius = radius;
-    var innerRadius = radius * 0.9;
-    for (var i = 0; i < 360; i += 12) {
-      var x1 = centerX + outerRadius * cos(i * pi / 180);
-      var y1 = centerY + outerRadius * sin(i * pi / 180);
-
-      var x2 = centerX + innerRadius * cos(i * pi / 180);
-      var y2 = centerY + innerRadius * sin(i * pi / 180);
-      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
-    }
   }
 
   @override
